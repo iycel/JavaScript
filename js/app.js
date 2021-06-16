df9a88504b90673816e54f81cb291a4a5b10bb52
iycel <ismailycel@gmail.com>
2021-06-16 22:39:58 +0300

    s02

---
+++ js/app.js
@@ -0,0 +1,65 @@
+// console.log("Hello from app.js");
+// console.info("Some info");
+// console.warn("some warning");
+// console.error("error !!!")
+// console.time()
+// console.log("_______");
+// var myName = "Ismail";
+// console.log(myName);
+// var num = 4;
+// console.log(num * num);
+// myName = "Yucel"
+// console.log(myName);
+// var announce = "Welcome to our school!!"
+// console.log(announce);
+// annuonce = "Welcome to Full-Stack"
+// console.log(announce);
+
+// const PI = 3.141521;
+// const SCHOOL = "Clarusway of IT School";
+// const DB_SERVER = "192.168.11.22:5432/mydb"
+// let PORT = 3000;
+// // let myNum = 5;
+// console.log(PORT);
+// // myNum = PI * 4;
+// PORT = 3;
+// console.log(PORT);
+
+// const SECONDS_IN_A_DAY = 24*60*60;
+// console.log(SECONDS_IN_A_DAY);
+
+// // alert("Merhaba");
+// // window.alert("Merhaba");
+
+// var country = prompt("Enter Your Country");
+
+// console.log("Your country is", country);
+
+// var num1 = 42;
+// console.log(num1, num2);
+// if (num1==42){
+//     var num2 = 22;
+//     console.log(num1, num2);
+// }
+// console.log(num1, num2);
+// var num2 = 88;
+// console.log(num1, num2);
+
+// let num1 = 42;
+// console.log(num1 ); //,num2);
+// if (num1==42){
+//     let num2 = 22;
+//     console.log(num1, num2);
+// }
+// console.log(num1, num2);
+// num2 = 88;
+// console.log(num1, num2);
+
+function func1() {
+    var myName = "John";
+    console.log("from func1 ...", myName);
+    if (true){
+        var yourname="Melih"
+    }
+}
+// console.log("from func1 ...");
